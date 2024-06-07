72d7f7de4f84ee969a27cb51a6f6a92c245874de
Louis Fabre <louis.nsi2@sturmel.com>
2024-06-07 19:23:12 +0200

    Modularisation : create function for each small features in the previous extract_book(url) function

--- Scraper.py
+++ Scraper.py
@@ -4,23 +4,11 @@ import csv
 
 
 # For one book
-# Load
-
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-
-def extract_book(url):
-    infos_to_csv ={}
-
-# add product url to info_to_csv dictionary   
-    infos_to_csv["product_page_url"] = url
 
 
-# parse the web page
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
 
+def extract_product_informations(soup):
 
-# extract product informations
     product_informations = []
 
     trs = soup.find_all("tr")
@@ -28,42 +16,36 @@ def extract_book(url):
     for tr in trs:
         td = tr.find("td")
         product_informations.append(td.text)
-# add the desired product informations to infos_to_csv dictionary
-    infos_to_csv["universal_ product_code"] = product_informations[0]
-    infos_to_csv["price_excluding_tax"] = product_informations[2]
-    infos_to_csv["price_including_tax"] = product_informations[3]
-    infos_to_csv["number_available"] = product_informations[5]
     
+    
+    return product_informations
 
-# extract category
+def extract_category(soup):
     breadcrumb = []
 
     links = soup.find("ul", {"class": "breadcrumb"}).find_all("a")
     for link in links:
         breadcrumb.append(link.text)
-# extract title
+    
+    return breadcrumb    
+
+def extract_title(soup):
     title = soup.find("li", {"class": "active"})
-    breadcrumb.append(title.text)
-# add the title and category to infos_to_csv dictionary
-    infos_to_csv["title"] = breadcrumb[-1]
-    infos_to_csv["category"] = breadcrumb[-2]
-
-
-
-# extract image's url
+    
+    return title.text
+    
+def extract_image_url(soup):
     div = soup.find("div", {"class": "item active"}).find("img")
     image_url = div.get("src")
     
-    infos_to_csv["image_url"] = image_url
+    return image_url   
 
-
-# extract the product description   
+def extract_product_description(soup):   
     product_description = soup.find("article", {"class": "product_page"}).find("p", recursive=False)
-        
-    infos_to_csv["product_description"] = product_description.text
     
-
-# extract the review rating
+    return product_description.text
+    
+def extract_review_rating(soup):
     str_to_int = {
         "One":1 ,
         "Two":2,
@@ -75,18 +57,47 @@ def extract_book(url):
     rating_class = rating_class["class"]
     rating_class = rating_class[1]
     review_rating = str_to_int[rating_class]
+
+    return review_rating
     
-    infos_to_csv["review_rating"] = review_rating
-    print(review_rating)
+    
+    
 
+    
+    
+    
+def extract_book(url):
+    infos_to_csv ={}
 
+    # parse the web page
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # add product url to info_to_csv dictionary   
+    infos_to_csv["product_page_url"] = url
+
+    extract_product_informations(soup)
+    infos_to_csv["universal_ product_code"] = extract_product_informations(soup)[0]
+    infos_to_csv["price_excluding_tax"] = extract_product_informations(soup)[2]
+    infos_to_csv["price_including_tax"] = extract_product_informations(soup)[3]
+    infos_to_csv["number_available"] = extract_product_informations(soup)[5]
+
+    extract_category(soup)
+    infos_to_csv["category"] = extract_category(soup)[-1]
+
+    extract_title(soup)
+    infos_to_csv["title"] = extract_title(soup)
+
+    extract_image_url(soup)
+    infos_to_csv["image_url"] = extract_image_url(soup)
+
+    extract_product_description(soup)
+    infos_to_csv["product_description"] = extract_product_description(soup)
+
+    extract_review_rating(soup)
+    infos_to_csv["review_rating"] = extract_review_rating(soup)
     
     return infos_to_csv
-    #print(infos_to_csv)
-
-
-infos_to_csv = extract_book(url)
-
 
 def create_csv(infos_to_csv):
     headers = ["product_page_url", "universal_ product_code", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
@@ -95,7 +106,11 @@ def create_csv(infos_to_csv):
         writer = csv.DictWriter(csv_file, fieldnames=headers)
         writer.writeheader()
         writer.writerow(infos_to_csv)
-        
+
+url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+
+extract_book(url)
+infos_to_csv = extract_book(url)        
 create_csv(infos_to_csv)
 
 
