5b833bf1ada70923dd3b5575b058c9a9996f7baf
Louis Fabre <louisfabre@hotmail.fr>
2024-06-07 11:42:21 +0200

    add scraped title and category to infos_to_csv dictionary

--- Scraper.py
+++ Scraper.py
@@ -17,30 +17,35 @@ def extract_book(url):
     soup = BeautifulSoup(response.text, "html.parser")
 
 
-    # find product information
-    product_information = []
+# extract product informations
+    product_informations = []
 
     trs = soup.find_all("tr")
 
     for tr in trs:
         td = tr.find("td")
-        product_information.append(td.text)
-
-    infos_to_csv["universal_ product_code"] = product_information[0]
-    infos_to_csv["price_excluding_tax"] = product_information[2]
-    infos_to_csv["price_including_tax"] = product_information[3]
-    infos_to_csv["number_available"] = product_information[5]
+        product_informations.append(td.text)
+# add the desired product informations to infos_to_csv dictionnary
+    infos_to_csv["universal_ product_code"] = product_informations[0]
+    infos_to_csv["price_excluding_tax"] = product_informations[2]
+    infos_to_csv["price_including_tax"] = product_informations[3]
+    infos_to_csv["number_available"] = product_informations[5]
     
 
-    # find title and category
-    nav = []
+# extract category
+    breadcrumb = []
 
     ul = soup.find("ul", {"class": "breadcrumb"})
     links = ul.find_all("a")
     for link in links:
-        nav.append(link.text)
+        breadcrumb.append(link.text)
+# extract title
+    title = soup.find("li", {"class": "active"})
+    breadcrumb.append(title.text)
+# add the title and category to infos_to_csv dictionnary
+    infos_to_csv["title"] = breadcrumb[-1]
+    infos_to_csv["category"] = breadcrumb[-2]
     
-    print(nav)
 
 
     print(infos_to_csv)
@@ -53,14 +58,14 @@ extract_book(url)
 
 
 """
-product_page_url                
-0 universal_ product_code (upc)
-title
-3 price_including_tax
-2 price_excluding_tax
-5 number_available
+- product_page_url                
+- 0 universal_ product_code (upc)
+- title
+- 3 price_including_tax
+- 2 price_excluding_tax
+- 5 number_available
 product_description
-category
+- category
 review_rating
 image_url
 """
