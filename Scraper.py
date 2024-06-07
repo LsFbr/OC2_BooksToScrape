d99814835efac4ba7af21f75b51a06c65940cfa6
Louis Fabre <louisfabre@hotmail.fr>
2024-06-07 11:04:31 +0200

    find category from breadcrumb

--- Scraper.py
+++ Scraper.py
@@ -1,26 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 # Pour un Livre
+# Load
+
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-response = requests.get(url)
-page=response.content
-soup = BeautifulSoup(page, "html.parser")
-infos =[]
-trs = soup.find_all("tr")
-for tr in trs:
-    td = tr.find("td")
-    infos.append(td)
-print(infos)
+
+def extract_book(url):
+    infos_to_csv ={}
+    
+    infos_to_csv["product_page_url"] = url
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+
+    # find product information
+    product_information = []
+
+    trs = soup.find_all("tr")
+
+    for tr in trs:
+        td = tr.find("td")
+        product_information.append(td.text)
+
+    # find title and category
+    nav = []
+
+    ul = soup.find("ul", {"class": "breadcrumb"})
+    links = ul.find_all("a")
+    for link in links:
+        nav.append(link.text)
+    
+    print(nav)
+
+
+    print(infos_to_csv)
+
+extract_book(url)
+
+
+
+
+
 
 """
-product_page_url
-universal_ product_code (upc)
+product_page_url                
+0 universal_ product_code (upc)
 title
-price_including_tax
-price_excluding_tax
-number_available
+3 price_including_tax
+2 price_excluding_tax
+5 number_available
 product_description
 category
 review_rating
